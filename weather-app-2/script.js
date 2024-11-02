af133bb2598babbcee7518beccf628da14e33830
Taha <tahaferhani1@gmail.com>
2024-11-02 20:39:48 +0100

    Multiple projects

--- weather-app-2/script.js
+++ weather-app-2/script.js
@@ -1,6 +1,40 @@
+const cities = [
+    {
+        id: 1,
+        name: "Paris",
+        latitude: 48.856613,
+        longitude: 2.352222,
+        image: "/images/paris.jpg"
+    },
+    {
+        id: 2,
+        name: "Shanghai",
+        latitude: 31.230391,
+        longitude: 121.473701,
+        image: "/images/shanghai.jpg"
+    },
+    {
+        id: 3,
+        name: "London",
+        latitude: 40.741895,
+        longitude: -73.989308,
+        image: "/images/london.jpg"
+    },
+    {
+        id: 4,
+        name: "Casablanca",
+        latitude: 33.5945144,
+        longitude: -7.6200284,
+        image: "/images/casablanca.jpg"
+    }
+];
+
 $("#cities").change(function() {
-    let coords = $(this).val().split(",");
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${coords[0]}&longitude=${coords[1]}&current=temperature_2m,is_day,rain&daily=temperature_2m_max,temperature_2m_min,rain_sum&timezone=auto`;
+    const id = $(this).val();
+    const city = cities.find(item => item.id == id);
+    $("#body-bg, #card-bg").css("background-image", `url(${city.image})`);
+
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${city.latitude}&longitude=${city.longitude}&current=temperature_2m,is_day,rain&daily=temperature_2m_max,temperature_2m_min,rain_sum&timezone=auto`;
 
     $.get(url, function(data) {
         $("#temp").html(data.current.temperature_2m + " C°");
@@ -16,16 +50,4 @@ $("#cities").change(function() {
         }
     })
 })
-
-// .value .val()
-// .value = "test"  .val("test")
-
-// .parentElement .parent()
-// .nextElementSibling .next() .nextAll()
-// .previousElementSibling .prev()  .prevAll()
-
-// .innerHTML   .html()
-// .innerHTML = "Test"  .html("Test")
-// .textContent .text()
-// .textContent = "Test"  .text("Test")
 
