811e9ab7b90f680b2b6d3e11c75ab73deeba1aff
Taha <tahaferhani1@gmail.com>
2025-04-17 16:48:58 +0100

    Products page

--- first-app-2/components/Input.jsx
+++ first-app-2/components/Input.jsx
@@ -1,13 +1,19 @@
 import { StyleSheet, TextInput } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Input = ({ placeholder, password, ...props }) => {
+    const [isFocused, setIsFocused] = useState(false);
+
     return (
         <TextInput
-            style={styles.input}
+            style={[styles.input, isFocused && {
+                borderColor: "#1F41BB"
+            }]}
             placeholder={placeholder}
             placeholderTextColor="#626262"
             secureTextEntry={password}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
             {...props} />
     )
 }
@@ -21,6 +27,8 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         fontSize: 16,
         fontFamily: "Poppins_500Medium",
-        marginBottom: 30
+        marginBottom: 30,
+        borderWidth: 2,
+        borderColor: "transparent"
     }
 })
